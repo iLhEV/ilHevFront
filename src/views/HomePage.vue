8736049c7c90fa4ec885b567703fa03089eb864b
ilheV <iadomyshev@gmail.com>
2022-05-14 18:23:54 +0400

    colors

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -21,7 +21,7 @@
         </v-menu>
 
         <h1 class="title-box font-weight-light text-center grow">
-          <span class="title-box__middle">~ ~ ~ ilHeV ~ ~ ~</span>
+          <span class="title-box__middle">~ ~ ~ ilheV ~ ~ ~</span>
         </h1>
         <v-avatar @click="openAuthDialog" class="login-avatar">
           <v-img
