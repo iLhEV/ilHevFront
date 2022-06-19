71eccab02147a3aec56948918a95479dbb5fbbc6
ilheV <iadomyshev@gmail.com>
2022-06-19 12:41:57 +0400

    improve examples layout

--- src/views/HtmlExamples.vue
+++ src/views/HtmlExamples.vue
@@ -1,60 +1,87 @@
 <template>
   <div class="pa-10">
-    <router-link :to="ROUTES.HOME">Домой</router-link>
-    <router-link :to="ROUTES.ARTICLES" class="pl-5">Статьи</router-link>
-    <v-divider class="my-3" style="border-color: #ccc" />
-    <v-row>
-      <v-col class="example-1 example">
-        <div class="description">
-          <h2>#1 - выравнивание по горизонтали и вертикали одновременно</h2>
-          <v-divider />
-          достигается за счёт назначения родительскому контейнеру трёх
-          css-свойств:<br />
-          display: flex;<br />
-          justify-content: center;<br />
-          align-items: center;.
-          <v-divider />
-          Первое свойство делает контейнер flex-контейнером. Второе
-          устанавливает выравнивание по-горизонтали. Третье - по-вертикали.
-          <v-divider />
-        </div>
-        <div class="example-body">
-          <div class="level-1">
-            <div class="level-2"></div>
+    <div class="menu-container">
+      <router-link :to="ROUTES.HOME">Домой</router-link>
+      <router-link :to="ROUTES.ARTICLES" class="pl-5">Статьи</router-link>
+    </div>
+    <div class="examples-area">
+      <v-row class="row-wrapper">
+        <v-col class="cell-wrapper">
+          <div class="example-container example-1">
+            <div class="description">
+              <h2>
+                #1 - выравнивание по горизонтали<br />
+                и вертикали одновременно
+              </h2>
+              <div class="example-description-wrapper">
+                <div class="example-description">
+                  Достигается за счёт назначения родительскому контейнеру трёх
+                  css-свойств:<br />
+                  display: flex;<br />
+                  justify-content: center;<br />
+                  align-items: center;.
+                  <v-divider />
+                  Первое свойство делает контейнер flex-контейнером. Второе
+                  устанавливает выравнивание по-горизонтали. Третье -
+                  по-вертикали.
+                </div>
+              </div>
+            </div>
+            <div class="example-body">
+              <div class="level-1">
+                <div class="level-2"></div>
+              </div>
+            </div>
           </div>
-        </div>
-      </v-col>
-      <v-col class="example-2 example">
-        <h2>#2 - выравнивание только по горизонтали</h2>
-        <v-divider />
-        Используется два css-свойства:<br />
-        display: flex;<br />
-        justify-content: center;
-        <v-divider />
-        Второе свойство устанавливает выравнивание по-горизонтали.
-        <v-divider />
-        <div class="example-body">
-          <div class="level-1">
-            <div class="level-2"></div>
+        </v-col>
+        <v-col class="cell-wrapper">
+          <div class="example-container example-2">
+            <h2>#2 - выравнивание только по горизонтали</h2>
+            <div class="example-description-wrapper">
+              <div class="example-description">
+                Используется два css-свойства:<br />
+                display: flex;<br />
+                justify-content: center;
+                <v-divider />
+                Второе свойство устанавливает выравнивание по-горизонтали.
+              </div>
+            </div>
+            <div class="example-body">
+              <div class="level-1">
+                <div class="level-2"></div>
+              </div>
+            </div>
           </div>
-        </div>
-      </v-col>
-      <v-col class="example-3 example">
-        <h2>#3 - выравнивание только по вертикали</h2>
-        <v-divider />
-        Используется два css-свойства:<br />
-        display: flex;<br />
-        align-items: center;
-        <v-divider />
-        Второе свойство устанавливает выравнивание по-вертикали.
-        <v-divider />
-        <div class="example-body">
-          <div class="level-1">
-            <div class="level-2"></div>
+        </v-col>
+        <v-col class="cell-wrapper">
+          <div class="example-container example-3">
+            <h2>#3 - выравнивание только по вертикали</h2>
+            <div class="example-description-wrapper">
+              <div class="example-description">
+                Используется два css-свойства:<br />
+                display: flex;<br />
+                align-items: center;
+                <v-divider />
+                Второе свойство устанавливает выравнивание по-вертикали.
+              </div>
+            </div>
+            <div class="example-body">
+              <div class="level-1">
+                <div class="level-2"></div>
+              </div>
+            </div>
           </div>
-        </div>
-      </v-col>
-    </v-row>
+        </v-col>
+      </v-row>
+      <v-row class="row-wrapper">
+        <v-col class="cell-wrapper">
+          <div class="example-container example-4"></div>
+        </v-col>
+        <v-col class="cell-wrapper">
+          <div class="example-container example-5"></div>
+        </v-col>
+      </v-row>
+    </div>
   </div>
 </template>
 
@@ -72,10 +99,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.menu-container {
+}
+.examples-area {
+  padding-top: 30px;
+}
 h2 {
   font-size: 15px;
+  min-height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 10px;
+  border-bottom: 1px solid #ddd;
+  text-align: center;
 }
-.row hr {
+hr {
   margin-top: 5px;
   margin-bottom: 5px;
 }
@@ -94,9 +133,6 @@ h2 {
     height: 150px;
     background: #a1887f;
   }
-}
-.example {
-  border-left: 1px solid #ccc;
 }
 .example-body {
   margin-top: 15px;
@@ -130,5 +166,33 @@ h2 {
     height: 150px;
     background: #a1887f;
   }
+}
+.cell-wrapper {
+  border-bottom: 1px solid #a1887f;
+}
+.cell-wrapper {
+  border-right: 1px solid #a1887f;
+}
+.cell-wrapper:first-child {
+  border-left: 1px solid #a1887f;
+}
+.row-wrapper:first-child .cell-wrapper {
+  border-top: 1px solid #a1887f;
+}
+.example-body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.example-description-wrapper {
+  display: flex;
+  min-height: 230px;
+  padding-top: 8px;
+  padding-bottom: 10px;
+  border-bottom: 1px solid #ddd;
+  align-items: center;
+}
+.example-description {
+  text-align: center;
 }
 </style>
